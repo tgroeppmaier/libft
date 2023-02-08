fc6f33fc31152b7c1c3dce36443aae46ad12aff3
tgroeppm <tgroeppm@student.42.fr>
2023-02-08 21:16:37 +0100

    fdgdfgh

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,17 +6,28 @@
 /*   By: tgroeppm <tgroeppm@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 18:43:32 by tgroeppm          #+#    #+#             */
-/*   Updated: 2023/02/08 20:49:26 by tgroeppm         ###   ########.fr       */
+/*   Updated: 2023/02/08 21:16:13 by tgroeppm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 t_list	*ft_lstlast(t_list *lst)
+/*
 {
 	if (!lst)
 		return (NULL);
 	while (lst->next)
 		lst = lst->next;
 	return (lst);
+} */
+{
+	t_list *current;
+
+	current = lst;
+	if (lst == NULL)
+		return (NULL);
+	while (current->next)
+		current = current->next;
+	return (current);
 }
